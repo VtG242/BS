631c3014f766b9154d336a7f8765f56977e36a3e
VtG <vladimir.volcko@gmail.com>
2017-09-19 18:32:46 +0200

    More human version

--- battleship.py
+++ battleship.py
@@ -1,48 +1,108 @@
 #!/usr/bin/python
-
 from random import randint
 
-board = []
+debug = False
 
-for x in range(5):
-  board.append(["O"] * 5)
+#vizualizace boardu
+board = [
+["5", "O", "O", "O", "O", "O"],
+["4", "O", "O", "O", "O", "O"],
+["3", "O", "O", "O", "O", "O"],
+["2", "O", "O", "O", "O", "O"],
+["1", "O", "O", "O", "O", "O"],
+["-", "A", "B", "C", "D", "E"],
+]
+x_map = {
+"A": 1,
+"B": 2,
+"C": 3,
+"D": 4,
+"E": 5,
+}
+y_map = {
+"1": 4,
+"2": 3,
+"3": 2,
+"4": 1,
+"5": 0,
+}
 
 def print_board(board):
-  for row in board:
-    print " ".join(row)
+    for row in board:
+        print " ".join(row)
 
-print_board(board)
-
+# umisteni lode - funkce
 def random_row(board):
-  return randint(0, len(board) - 1)
-
+    #dostupne hodnoty 0,1,2,3,4 (y_map)
+    return randint(0, len(board) - 2)
 def random_col(board):
-  return randint(0, len(board[0]) - 1)
+    #dostupne hodnoty 1,2,3,4,5 (x_map)
+    return randint(1, len(board[0]) - 1)
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-#print ship_row
-#print ship_col
 
-for turn in range(4):
-  # Everything from here on should go in your for loop!
-  # Be sure to indent four spaces!
-  guess_row = int(raw_input("Guess Row: "))
-  guess_col = int(raw_input("Guess Col: "))
-
-  if guess_row == ship_row and guess_col == ship_col:
-    print "Congratulations! You sunk my battleship!"
-    break
-  else:
-    if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-      print "Oops, that's not even in the ocean."
-    elif(board[guess_row][guess_col] == "X"):
-      print "You guessed that one already."
-    else:
-      print "You missed my battleship!"
-      board[guess_row][guess_col] = "X"
-    print "Turn", turn + 1
-    print_board(board)
+#debug:
+if debug == True:
+    print("Radky:" + str(len(board)))
+    print("Sloupce:" + str(len(board[0])))
+    print "Ship in - Row: " + str(ship_row) + " Col: " + str(ship_col)
+    board[ship_row][ship_col] = "S"
+        
+#zobrazeni boardu
+print_board(board)
     
-    if turn==3:
-      print "Game Over"
+#main game
+for turn in range(4):
+    
+    print "Turns remain: ", 4 - turn 
+
+    #user data
+    while True:
+        try:
+            guess_row = int(raw_input("Guess Row (number): "))
+            #radka v poli odpovidajici vizualizaci
+            guess_row = y_map[str(guess_row)]
+            if debug:
+                print guess_row    
+            break            
+        except:
+            ValueError
+            KeyError
+            print("That's not an integer or it's out of range!")
+                
+    while True:
+        guess_col = raw_input("Guess Col (letter): ")
+        try:
+            #skutecny sloupec v poli
+            guess_col = x_map[guess_col]
+            if debug:
+                print guess_col
+            break
+        except KeyError:
+            print("That's not a letter in range 'A - F'")
+
+    if guess_row == ship_row and guess_col == ship_col:
+        print "Congratulations! You sunk my battleship!"
+        break
+    else:
+        if(board[guess_row][guess_col] == "X"):
+            print "You guessed that one already."
+        else:
+            print "You missed my battleship!"
+            board[guess_row][guess_col] = "X"
+            print_board(board)
+    
+    if turn == 3:
+        print "Game Over"
+        
+        question = raw_input("Do you want to reveal a ship position? (Y/N)")
+        if question == "Y":
+            board[ship_row][ship_col] = "S"
+            print_board(board)
+        else:
+            print("Bye!")
+
+
+
+
